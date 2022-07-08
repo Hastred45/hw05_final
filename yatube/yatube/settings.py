a5959220aabef576e9d9d637ba8404f212ace489
Сергей Осетров <hastred45@yandex.ru>
2022-07-08 16:37:34 +0500

    debug

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd7ctg6ml#v6i)i*j-q-a2s^ooe4e!fcnyvzqdo4-(1w70i8f^x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '158.160.1.7'
